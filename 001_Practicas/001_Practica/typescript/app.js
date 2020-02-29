b6663916877cd922e628d299a15cecb26aa66d6c
Nestor Fernando Chocue <nfchocue@cmsoftlutions.com>
2020-02-29 11:48:47 -0500

    Funciones flecha

--- 001_Practicas/001_Practica/typescript/app.js
+++ 001_Practicas/001_Practica/typescript/app.js
@@ -1,11 +1,3 @@
 "use strict";
-var miFuncion = function (a) {
-    return a.toUpperCase();
-};
-var miFuncionF = function (a) { return a.toUpperCase(); };
-var sumarN = function (a, b) {
-    return a + b;
-};
-var sumarNF = function (a, b) { return a + b; };
-console.log("sumarN: " + sumarN(5, 5));
-console.log("sumarNF: " + sumarNF(6, 5));
+(function () {
+});
--- 001_Practicas/001_Practica/typescript/app.ts
+++ 001_Practicas/001_Practica/typescript/app.ts
@@ -1,17 +1,3 @@
-   const miFuncion = function(a:string){
-        return a.toUpperCase();
-   }
+(()=>{
 
-   const miFuncionF = (a:string) => a.toUpperCase();
-
-   const sumarN = function (a:number,b:number){
-       return a+b;
-   }
-
-   const sumarNF = (a:number, b:number) => a+b;
-   
-   
-   console.log(`sumarN: ${sumarN(5,5)}`);
-   console.log(`sumarNF: ${sumarNF(6,5)}`);
-
-
+})
