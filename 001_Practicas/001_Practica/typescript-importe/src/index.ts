b4a0e227d562b914e5c9d47191c7772d13555c74
nchocue <nestorchocue@gmail.com>
2020-03-13 20:47:02 -0500

    importación de proyectos

---
+++ 001_Practicas/001_Practica/typescript-importe/src/index.ts
@@ -0,0 +1,10 @@
+
+/*
+const mensaje = 'Hola Mundo!';
+
+console.log( mensaje );
+*/
+import {Xmen} from './classes/xmen.class';
+const wolverine = new Xmen('Nestor','ninguno');
+//console.log(wolverine);
+wolverine.imprimir();
